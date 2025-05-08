60d9fedf4610e3ba044354eebdc1e79b1082a879
Chris <chris.goringe@gmail.com>
2025-05-08 23:11:13 +1000

    int

--- image_filter.py
+++ image_filter.py
@@ -81,7 +81,7 @@ class ImageFilter(PreviewImage):
             images_to_return = [ key*video_frames + frm  for key in images_to_return for frm in range(video_frames)   ]
 
         images = torch.stack(list(images[int(i)] for i in images_to_return))
-        latents = {"samples": torch.stack(list(latents['samples'][i] for i in images_to_return))} if latents is not None else None
+        latents = {"samples": torch.stack(list(latents['samples'][int(i)] for i in images_to_return))} if latents is not None else None
         masks = torch.stack(list(masks[i] for i in images_to_return)) if masks is not None else None
                 
         return (images, latents, masks, e1, e2, e3, ",".join(str(x) for x in images_to_return))
