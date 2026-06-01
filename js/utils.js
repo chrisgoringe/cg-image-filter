694f8444e67f44d601861c5604bb3e55c35daf9d
Chris <chris.goringe@gmail.com>
2026-06-01 20:11:02 +1000

    add periodic dings

--- js/utils.js
+++ js/utils.js
@@ -6,3 +6,28 @@ export function create( tag, clss, parent, properties ) {
     if (properties) Object.assign(nd, properties);
     return nd;
 }
+
+export class CallbackThrottle {
+    constructor(callback, min_period) {
+        this.callback = callback
+        this.millisecs = min_period*1000
+        this.unreset()
+    }
+
+    reset() { 
+        this.last_reset = Date.now()
+    }
+
+    unreset() {
+        this.last_reset = Date.now() - this.millisecs
+    }
+
+    request() {
+        const elapsed = Date.now()-this.last_reset
+        if (Date.now()-this.last_reset > this.millisecs) {
+            console.log(`Callback`)
+            this.callback()
+            this.reset()
+        }
+    }
+}
